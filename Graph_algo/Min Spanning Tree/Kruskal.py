64c19919e2f8158d01d2ac821a058d553c7d63e0
Peienn <92717166+Peienn@users.noreply.github.com>
2025-07-30 14:22:54 +0800

    Update and rename Graph_algo/Kruskal.py to Graph_algo/Min Spanning Tree/Kruskal.py

--- Graph_algo/Min Spanning Tree/Kruskal.py
+++ Graph_algo/Min Spanning Tree/Kruskal.py
@@ -1,44 +1,31 @@
 class UF:
-    
     def __init__(self,nodes):
         self.parent = {node:node for node in nodes}
-        
     def find(self,node):
-        
         if self.parent[node]!=node:
-            
             self.parent[node] = self.find(self.parent[node])
         return self.parent[node]
-
     def union(self,u,v):
         root1 = self.find(u)
         root2 = self.find(v)
-        
         if root1==root2:
-            
             return False
         
         self.parent[root2] = root1
         return True
-        
+
 
 def krustal(graph):
-    
     edges=[]
     nodes=set()
     total_w=0
     mst=[]
-    for node in graph:
-        
+    for node in graph: 
         for neighbor,edge in graph[node].items():
-            
             if (node,neighbor) not in edges:
-                
                 edges.append(  (node,neighbor,edge))
-                
             nodes.add(node)
             nodes.add(neighbor)
-            
     edges.sort(key=lambda x:x[2])
 
     uf = UF(nodes)
@@ -51,3 +38,17 @@ def krustal(graph):
             total_w +=w            
     
     return mst,total_w
+
+
+
+
+
+if __name__ == '__main__':
+    graph = {
+        'A': {'B': 4, 'C': 1, 'D': 3},
+        'B': {'A': 4, 'D': 2},
+        'C': {'A': 1, 'D': 5 , 'E':10},
+        'D': {'A': 3, 'B': 2, 'C': 5, 'E': 6},
+        'E': {'C':10, 'D': 6}
+    }
+    mst,total_w = krustal(graph)
